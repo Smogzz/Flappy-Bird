154ed6cc33d67a62286470d5f1ceed9798a78a94
yariiiiii <yvega25@aoiths.org>
2024-04-11 08:49:25 -0400

    pipe spawning

---
+++ Assets/Script/Spawn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject pipePrefab;
+    float minY = -1.99f;
+    float maxY = -1.06f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawnPipes", 2.0f, 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnPipes()
+    {
+        Instantiate(pipePrefab, new Vector2(14.12f,Random.Range(minY,maxY)),Quaternion.identity);
+    }
+    
+
+}
+
